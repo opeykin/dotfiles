18c50a28cfd2c57e681f0a8e512e232ce98f198d
Alexander Opeykin <alexander.opeykin@gmail.com>
2026-01-03 07:52:17 -0800

    fzf: fix ctrl+g for hg sl

--- fzf.bash
+++ fzf.bash
@@ -20,17 +20,17 @@ if [ -f /usr/share/fzf/shell/key-bindings.bash ]; then
 fi
 
 # hg sl
-# ------------
 hg_sl_fzf() {
-    # hg sl | fzf --phony --reverse --height=50% | sed -E 's/.*[o@x]\s*([0-9a-f]*)\s.*/\1/'
     hg sl | fzf --phony --reverse --height=50% | sed -E 's/.*[o@x]\s*([0-9a-f]+)\s.*/\1/'
-    #script --return --quiet -c "hg sl" /dev/null | sed 's/\x1B\[\?[0-9]*[lh]//g' | fzf --phony --reverse --height=50% | sed -E 's/.*[o@x]\s*([0-9a-f]+)\s.*/\1/'
 }
 
+# Wrapper function for readline binding
+_hg_sl_fzf_widget() {
+    local result
+    result=$(hg_sl_fzf)
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${result}${READLINE_LINE:$READLINE_POINT}"
+    READLINE_POINT=$((READLINE_POINT + ${#result}))
+}
 
-if [[ ! -o vi ]]; then
-    # this is not tested. There is 80% it's going to work
-    bind '"\C-g": " \C-e\C-u\C-y\ey\C-u`hg_sl_fzf`\e\C-e\er\e^"'
-else
-    bind '"\C-g": "\C-x\C-a$a \C-x\C-addi`hg_sl_fzf`\C-x\C-e\C-x\C-a0Px$a \C-x\C-r\C-x\C-axa "'
-fi
+# Bind Ctrl+g to the widget
+bind -x '"\C-g": _hg_sl_fzf_widget'
